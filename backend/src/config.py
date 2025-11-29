83cadad3129ce2e194dc6e63ade47eb8fcd650bf
Sarthak Dargan <sarthakdargan@Sarthaks-MacBook-Air.local>
2025-11-29 19:23:36 +0530

    Added Render Yaml

---
+++ backend/src/config.py
@@ -0,0 +1,23 @@
+from pydantic_settings import BaseSettings
+from typing import List, Optional
+
+class Settings(BaseSettings):
+    APP_NAME: str = "ResumeForge"
+    VERSION: str = "1.0.0"
+    DEBUG: bool = True
+    CORS_ORIGINS: List[str] = ["http://localhost", "http://localhost:3000", "https://resumegpt-frontend.vercel.app"]
+
+    # OPENAI SETTINGS
+    OPENAI_API_KEY: str
+    
+    # Token limits
+    MAX_TOKENS_FOR_MODIFY: int = 16000  
+
+    # AUTH
+    CLERK_JWKS_URL: str
+   
+    class Config:
+        env_file = ".env"
+        extra = "ignore" # Ignore extra env vars
+
+settings = Settings()
